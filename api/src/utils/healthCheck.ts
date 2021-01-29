fdb673e375921de5209d3f2264937d521f3fecc7
Michael Johnson <michael@icefoganalytics.com>
2021-01-29 13:47:19 -0700

    Update healthCheck.ts

--- api/src/utils/healthCheck.ts
+++ api/src/utils/healthCheck.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import { Storage } from "../data";
-import { DB_HOST, DB_NAME } from "../config";
+import { MONGO_HOST } from "../config";
 
 export async function doHealthCheck(req: Request, res: Response) {
     let data = req.store as Storage;
     let dbConnected = await data.isConnected();
 
     if (!dbConnected)
-        return res.status(500).send(`Not able to connect to <strong>${DB_NAME}</strong> database on <strong>${DB_HOST}</strong>.`);
+        return res.status(500).send(`Not able to connect to <strong>MONGODB</strong> database on <strong>${MONGO_HOST}</strong>.`);
 
-    res.send(`Connection to database on '<strong>${DB_HOST}</strong>' is connected and functioning.`);
+    res.send(`Connection to database on '<strong>${MONGO_HOST}</strong>' is connected and functioning.`);
 }
