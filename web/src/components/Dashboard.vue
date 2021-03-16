d57501edf143e08d26d6aba2cbf5ac3ca08f90fb
Michael Johnson <michael@icefoganalytics.com>
2021-03-16 11:31:01 -0700

    Update Dashboard.vue

--- web/src/components/Dashboard.vue
+++ web/src/components/Dashboard.vue
@@ -3,20 +3,20 @@
     <h1>Dashboard</h1>
 
     <v-card class="mt-5" color="#fff2d5">
-      <v-card-title>Card with info</v-card-title>
+      <v-card-title>Upcoming Changes</v-card-title>
       <v-card-text>This is the body of the text</v-card-text>
     </v-card>
 
     <div class="row">
       <div class="col-md-6">
         <v-card class="mt-5" color="#fff2d5">
-          <v-card-title>Another card</v-card-title>
+          <v-card-title>Open Change Requests</v-card-title>
           <v-card-text>This is the body of the text</v-card-text>
         </v-card>
       </div>
       <div class="col-md-6">
         <v-card class="mt-5" color="#fff2d5">
-          <v-card-title>Yet another</v-card-title>
+          <v-card-title>Draft Entities</v-card-title>
           <v-card-text>This is the body of the text</v-card-text>
         </v-card>
       </div>
