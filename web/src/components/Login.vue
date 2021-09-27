ee454b46e08e88d11cabd9a9b5d6d9ea8da88ef0
Michael Johnson <michael@icefoganalytics.com>
2021-09-27 10:14:12 -0700

    Update text for Auth0

--- web/src/components/Login.vue
+++ web/src/components/Login.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="hello">
     <h1>{{ title }}</h1>
+    <h3>Please use your YNET credentials to sign in</h3>
     <p>
-      The authentication for this application is managed by our authentication
-      partner Vivvo. When you click the button below, you will be redirected to
-      their site and once authenticated, back here.
+      The authentication for this application is managed by an authentication
+      partner. When you click the button below, you will be redirected to their
+      site and once authenticated, back here.
     </p>
     <p>
-      If you have already authenticated with Vivvo and your session is still
-      active, it may skip the sign in process and return you here immediately.
+      If you have already authenticated and your session is still active, it may
+      skip the sign in process and return you here immediately.
     </p>
 
     <a class="v-btn primary v-size--default" :href="loginLink"
@@ -26,7 +27,7 @@ export default {
   name: "Login",
   data: () => ({
     loginLink: `${config.apiBaseUrl}/api/auth/login`,
-    title: `Sign in to ${config.applicationName}`
+    title: `Sign in to ${config.applicationName}`,
   }),
   async created() {
     await store.dispatch("checkAuthentication");
@@ -37,6 +38,6 @@ export default {
     }
 
     console.log(config.apiBaseUrl);
-  }
+  },
 };
 </script>
